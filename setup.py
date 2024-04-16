51c3399cc60157c014a452b35644f148436f8543
Petr Vanc <petr.vanc@cvut.cz>
2024-04-16 14:18:16 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'coppelia_sim_ros_interface_msgs'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='petr',
+    maintainer_email='petr.vanc@cvut.cz',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
